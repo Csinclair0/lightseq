46ca48719607e1501586b3d10d4ad6f761a3c823
Colin Sinclair <csinclair0@gmail.com>
2022-11-24 11:13:30 -0800

    update transformer

--- build_proto.sh
+++ build_proto.sh
@@ -1,3 +1,5 @@
-SRC_DIR="/Users/csinclair/src/lightseq/protoclightseq/inference/proto"
+SRC_DIR="/Users/csinclair/src/lightseq/lightseq/inference/proto"
 PY_DIR="/Users/csinclair/src/lightseq/examples/inference/python/export/proto"
 protoc -I=$SRC_DIR --python_out=$PY_DIR $SRC_DIR/moe.proto
+
+protoc -I=$SRC_DIR --python_out=$PY_DIR $SRC_DIR/transformer.proto
